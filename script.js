9fe9825b253b3f3e4c79905e169036bf15112088
brandocorti <brando.corti@studenti.unitn.it>
2024-10-14 21:52:07 +0200

    added Files

---
+++ script.js
@@ -0,0 +1,208 @@
+const quizSong = [
+    {
+        question: "A noi ci va così, così bene che non so che cosa dire tu diresti c'est la vie",
+        options:["Nitro - Pleasantville","Salmo - L'alba","Tedua - Vertigini", "Salmo - Perdonami"],
+        RightAnswer: "Salmo - L'alba"
+    },
+    {
+        question: "Quando è finita mi son detto 'Cosa vuoi che faccia?' In sta città non ce n'è una che in fondo mi piaccia.",
+        options:["Madman - Baby Blue", "Lazza - 24H", "Ernia - Bella", "Ernia - Domani"],
+        RightAnswer: "Ernia - Bella"
+    },
+    {
+        question: "Faremo un pic-nic sulla mia camicia Burberry, sperando che non piova come a Carnaby",
+        options:["Lazza - Re Mida", "Marracash - Scooteroni" , "Gue - Lamborghini", "Massimo Pericolo - AIRFORCE"],
+        RightAnswer: "Lazza - Re Mida"
+    },
+    {
+        question: "E' il giorno del mio matrimonio, Oggi mi sposo con Money, i soldi per me sono Dio ma per molti sono il demonio",
+        options:["Gue - Milionario", "Marracash - Sport", "Gue - Lamborghini", "Emis Killa - Montecarlo"],
+        RightAnswer: "Gue - Lamborghini"
+    },
+    {
+        question: "Vorrei che i miei giorni durassero 48 ore perche non mi bastano più la metà",
+        options:["Gue - 25 ore", "Izi - 48H", "Rkomi - 180","Lazza - 24H"],
+        RightAnswer: "Izi - 48H"
+    },
+    {
+        question: "Per non farmi legare il legale consiglia negare il legame illegale",
+        options:["Fabri Fibra - Il rap nel mio paese", "Ensi - Mamma diceva","Marracash - Salvador Dalì","DPG - British"],
+        RightAnswer: "Marracash - Salvador Dalì"
+    },
+    {
+        question: "Io sono diverso, ancora senza freni innamorato dell'eccesso",
+        options:["Gemitaiz - Un giro con noi","Gemitaiz - Diverso","Tony Effe - Diverso", "Geolier - Queen"],
+        RightAnswer: "Gemitaiz - Diverso"
+    },
+    {
+        question: "Saremo ricchi, ricchi per sempre, o forse no vabbè fa niente",
+        options:["Sfera Ebbasta - Ricchi x sempre","Ghali - Ricchi dentro","Sfera Ebbasta - Cupido","DPG - Fiori del male"],
+        RightAnswer: "Sfera Ebbasta - Ricchi x sempre"
+    },
+    {
+        question: "Ciao Amore, non piangere ti mando un bacio, scusami  ma a raggiungerti non ce la faccio lo sai com'è fatta Roma col traffico",
+        options:["Gemitaiz - Senza di me","Chiello - Acqua Salata", "Madman - Centro", "Nayt - Cosa conta davvero"],
+        RightAnswer: "Gemitaiz - Senza di me"
+    },
+    {
+        question: "Io non vado a dormire prima delle tre, poi mi sveglio e mi chiedo il sole dov'è?",
+        options:["Sfera Ebbasta -  BRNBQ", "Rkomi - Nuovo Range", "Ghali - Happy Days", "Rkomi - Milano Bachata"],
+        RightAnswer: "Ghali - Happy Days"
+    },
+    {
+        question: "La storia è questa, non ti resta che fare il critico, Non sei riuscito mai a fare l'artista",
+        options:["Gue - Voodoo", "Fabri Fibra - Vip in trip", "Madman - Kournikova", "Lazza - DDA"],
+        RightAnswer: "Gue - Voodoo"
+    },
+    {
+        question: "Federico, mi guardi su Wikipedia se sotto la voce di 'Rapper superfighissimo' c'è la mia faccia?",
+        options:["Fabri Fibra - Tranne te","Fabri Fibra - Vip in trip", "Marracash - Nulla accade", "Fabri Fibra - Controcultura"],
+        RightAnswer: "Fabri Fibra - Vip in trip"
+    },
+    {
+        question: "Baby con te ho fatto centro, amo il tuo look e il tuo accento, chiedimi tutto lo accetto",
+        options:["Coez - Taciturnal", "Lazza - Netflix", "Gemitaiz - Forte", "Madman - Centro"],
+        RightAnswer: "Madman - Centro"
+    },
+    {
+        question: "Ma poi queste tipe si innamorano, dice 'dormiamo insieme' dico 'ancora no'",
+        options:["Salmo Lunedi", "Gemitaiz - Toradol", "Bresh - Angelina Jolie", "Neima Ezza - Casa"],
+        RightAnswer: "Gemitaiz - Toradol"
+    },
+    {
+        question: "Sei solo tu qullo che ho, l'unica droga a cui non ho saputo dire no",
+        options:["Chic - Izi", "Gue - Eravamo re", "Capo Plaza - Non fare così", "Emis Killa - Fuoco e benzina"],
+        RightAnswer: "Emis Killa - Fuoco e benzina"
+    },
+    {
+        question: "Mamma me chiamme e nun rispongo, dice 'Addò staje?', nun m'arricordo",
+        options:["Geolier - Como te", "Luché - Non abbiamo età", "Emis Killa - Adios", "MV Killa - Luntan a me"],
+        RightAnswer: "Geolier - Como te"
+    }
+
+];
+
+let currentQuestion = 0;
+let score = 0;
+const maxQuestions = 10;
+let selectedQuestions = []
+
+const questionElement = document.getElementById('question');
+const optionsElement = document.getElementById('options');
+const resultElement = document.getElementById('result');
+
+
+function ShuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function selectRandomQuestions() {
+    selectedQuestions = ShuffleArray([...quizSong]).slice(0, maxQuestions);
+}
+
+function loadQuestion() {
+    if (currentQuestion >= selectedQuestions.length) {
+        displayFinalMessage();
+        return;
+    }
+    const currentQuestionObj = selectedQuestions[currentQuestion];
+
+    questionElement.textContent = currentQuestionObj.question;
+
+    optionsElement.innerHTML = '';
+
+
+    currentQuestionObj.options.forEach(option => {
+        const button = document.createElement('button');
+        button.textContent = option;
+        button.addEventListener('click', () => {
+            checkAnswer(option);
+        });
+        optionsElement.appendChild(button);
+        });
+}
+
+
+function checkAnswer(answer) {
+    const currentQuestionObj = selectedQuestions[currentQuestion];
+    const buttons = optionsElement.querySelectorAll('button');
+
+    buttons.forEach(button => {
+        button.disabled = true;
+        if (button.textContent === currentQuestionObj.RightAnswer){
+            button.classList.add('correct');
+        } else if (button.textContent === answer){
+            button.classList.add('wrong');
+        }
+    });
+
+    if (answer === currentQuestionObj.RightAnswer) {
+        score++;
+        resultElement.textContent = 'Correct! Your score is: '+ score;
+        /*setTimeout(() => {
+            resultElement.textContent = '';
+        }, 3000); */
+    } else {
+        resultElement.textContent = 'Wrong! The correct answer was: '+ currentQuestionObj.RightAnswer;
+        /*setTimeout(() => {
+            resultElement.textContent = '';
+        }, 3000); */
+    }
+    currentQuestion++;
+    setTimeout(loadQuestion, 2000);
+
+    /* if (currentQuestion < quizSong.length){
+        setTimeout(loadQuestion, 1000);
+    } else {
+        displayFinalMessage()
+    } */
+
+}
+
+function displayFinalMessage(){
+    optionsElement.innerHTML = ''; // rimuove le opzionip per presentare il messaggio finale
+
+    let finalMessage = '';
+    let spotifyLink = '';
+    let scoreClass= '';
+
+    const percentage = (score / selectedQuestions.length) * 100;
+
+    if (percentage < 50) {
+        finalMessage = `Game over! Your final score is ${score}. Better luck next time`;
+        spotifyLink = '<a href="https://open.spotify.com/playlist/6vwqf5dU6oBUCSYdIOMU4n?si=05f4d6077b194ef9" target="_blank"> You can study for the quiz here!</a>';
+        scoreClass = 'score-red'
+    } else if (percentage  > 50 && percentage < 70) {
+        finalMessage = `Congratulations! Your final score is ${score}. You did well!`;
+        spotifyLink = '<a href="https://open.spotify.com/playlist/6vwqf5dU6oBUCSYdIOMU4n?si=05f4d6077b194ef9" target="_blank"> If you liked the test, this is for you!</a>';
+        scoreClass = 'score-orange'
+    } else {
+        finalMessage = `Congratulations! Your final score is ${score}. You did very well!`;
+        spotifyLink = '<a href="https://open.spotify.com/playlist/6vwqf5dU6oBUCSYdIOMU4n?si=05f4d6077b194ef9" target="_blank"> If you liked the test, this is for you!</a>';
+        scoreClass = 'score-green'
+    }
+
+
+    resultElement.innerHTML =  `
+        <p>${finalMessage}</p> 
+        <p class="final-score ${scoreClass}">${score}</p>
+        <p>${spotifyLink}</p>
+     ` ;
+}
+
+function restartQuiz(){
+    currentQuestion = 0;
+    score = 0;
+    resultElement.textContent = '';
+    optionsElement.innerHTML = '';
+    selectRandomQuestions();
+    loadQuestion();
+}
+
+selectRandomQuestions();
+loadQuestion();
+
